8bac4ee9b18decd24a5d3237478fc6b20cb7bb67
Esteban RL <esteban.jrelei@gmail.com>
2019-09-22 14:55:19 -0600

    Creación de la base para la clase.

--- Blanco.java
+++ Blanco.java
@@ -1,4 +1,31 @@
-
 public class Blanco{
+	private Viento variacionPorViento;
+	private Jugador variacionPorNivel;
+	private double variacionTotal;
+	private int puntuacion;
+
+	public Blanco(){
+		puntuacion = 0;
+		variacionPorViento = new Viento();
+		variacionPorNivel = new Jugador();
+		//La línea siguiente es para usar unos datos de prueba porque aún no está la parte de dónde vienen los datos reales, lo que debería recibir realmente son las coordenadas en x y y.
+		variacionTotal = variacionPorViento.aplicarViento(0.6, true) + variacionPorNivel.tiro(0.6); 
+	}
+
+	public int asignarPuntuacion(){
+		if (variacionTotal > 0.4) {
+			puntuacion = 15;
+		}
+		else{
+			if (variacionTotal <= 0.4 && variacionTotal > 0.05) {
+				puntuacion = 20;
+			}
+			else{
+				puntuacion = 100;
+				
+			}
+		}
+		return puntuacion;
+	}
     
 }
